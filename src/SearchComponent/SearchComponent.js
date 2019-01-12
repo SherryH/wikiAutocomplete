145b53f0f78a56e6ea3201394bbde60aff89d04e
SherryH <enjoy.wind7rain@gmail.com>
2019-01-12 20:56:41 +1300

    Changed export structure of SearchComponent

---
+++ src/SearchComponent/SearchComponent.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import SearchBox from './SearchBox';
+import SearchContainer from './SearchContainer';
+
+const SearchComponent = ({ classes }) => (
+  <SearchContainer>
+    {({ isOpen, ...otherProps }) => (
+      <div className={classNames(classes.root, { open: isOpen })}>
+        <SearchBox isOpen={isOpen} {...otherProps} />
+      </div>
+    )}
+  </SearchContainer>
+);
+
+const useStyles = withStyles({
+  root: {
+    width: '100vw',
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    transition: '2s ease all',
+    '&.open': {
+      background: '#1eaddc',
+    },
+  },
+});
+
+export default useStyles(SearchComponent);
